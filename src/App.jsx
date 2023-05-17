5113779d08e4a914969dc8d2cfdd43cdf3806506
Anderson Silva <akgs_7@hotmail.com>
2023-05-17 19:39:20 -0300

    Alteração na chamada da const no App.

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ function App() {
         cargo: informacao.data.cargo
       });
 
-      const experienciaAcademica = await api.get(`/experiencias?tipo=academica`);
-      const experienciaProfissional = await api.get(`/experiencias?tipo=profissional`);
+      const experienciaAcademica = await api.get(`/experiencias?tipo=Academica`);
+      const experienciaProfissional = await api.get(`/experiencias?tipo=Profissional`);
 
       setCurriculo({
         sobre: informacao.data.sobre,
